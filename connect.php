bd42009d8fd9e44bbe5134a2daeef0e91940b346
Xinghai Ge <g18845381272@163.com>
2025-05-21 16:57:56 +0800

    connect to mysql

---
+++ connect.php
@@ -0,0 +1,17 @@
+<?php
+// connect.php
+$server = 'localhost:3306';
+$username = 'root';
+$password = '123.com';
+$database = 'db_forum';
+$conn = mysqli_connect($server, $username, $password, $database);
+
+// 检查连接
+if (!$conn) {
+    exit('Error: 无法连接到 MySQL.');
+}
+
+// 选择数据库
+if (!mysqli_select_db($conn, $database)) {
+    exit('Error: 无法选择数据库.');
+}
